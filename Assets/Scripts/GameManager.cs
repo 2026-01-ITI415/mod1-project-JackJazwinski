996ead57ab91e166f85fb3714ec851948006f7f0
JackJazwinski <jackjaz03@gmail.com>
2026-03-07 11:28:23 -0500

    Added a restart button to the UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,24 @@
 using UnityEngine;
-using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject gameOverText;
-
+    public GameObject restartButton;
     void Start()
     {
         gameOverText.SetActive(false);
+        restartButton.SetActive(false);
     }
-
     public void GameOver()
     {
         gameOverText.SetActive(true);
+        restartButton.SetActive(true);
         Time.timeScale = 0f;
+    }
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
